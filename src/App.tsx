3a4cbb08ee4d6eb0df14ab5ca26fcef9b4eddf3e
sahilpotdukhegriffy <sahilpotdukhe@griffy.app>
2024-09-21 13:14:30 +0530

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import WebApp from "@twa-dev/sdk";
-// import TelegramAuth from "./components/TelegramAuth";
-import { getSession } from "./utils/session";
 
 interface UserData {
   id: number;
@@ -13,37 +11,13 @@ interface UserData {
   is_premium?: boolean;
 }
 
-const App: React.FC = () => {
+function App() {
   const [userData, setUserData] = useState<UserData | null>(null);
-  // const [session, setSession] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
-
-  // Fetch user data from Telegram SDK and session from your server
-  // useEffect(() => {
-  //   const fetchSession = async () => {
-  //     try {
-  //       // Fetch session from your API or server
-  //       const sessionData = await getSession();
-  //       setSession(sessionData);
-  //     } catch (error) {
-  //       console.error("Error fetching session:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   if (WebApp.initDataUnsafe.user) {
-  //     setUserData(WebApp.initDataUnsafe.user as UserData);
-  //   }
-
-  //   // Fetch the session data
-  //   fetchSession();
-  // }, []);
-
-  // While loading, show a loading indicator
-  if (loading) {
-    return <div>Loading... Please Wait</div>;
-  }
+  useEffect(() => {
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData);
+    }
+  }, []);
 
   return (
     <>
@@ -53,7 +27,7 @@ const App: React.FC = () => {
           <ul>
             <li>ID: {userData.id}</li>
             <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name}</li>
+            <li>LastName: {userData.last_name}</li>
             <li>Username: {userData.username}</li>
             <li>Language Code: {userData.language_code}</li>
             <li>Is Premium: {userData.is_premium ? "Yes" : "No"}</li>
@@ -61,16 +35,10 @@ const App: React.FC = () => {
           </ul>
         </>
       ) : (
-        <div>No user data available</div>
+        <div>Loading... Please Wait</div>
       )}
-
-      {/* <h1 className="text-4xl font-bold mb-8">
-        Jwt Authentication for Telegram Mini Apps
-      </h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-      <TelegramAuth /> */}
     </>
   );
-};
+}
 
 export default App;
